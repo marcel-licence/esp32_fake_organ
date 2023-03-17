b9840ef89ae8cc6703aa42a9c035cb042c6461b5
Marcel Licence <marcel.licence@gmx.de>
2023-03-17 22:42:45 +0100

    Add some stubs which could be implemented by own needs

---
+++ display_module.ino
@@ -0,0 +1,97 @@
+/*
+ * display_module.ino
+ *
+ *  Created on: 05.07.2021
+ *      Author: PC
+ */
+
+#ifdef __CDT_PARSER__
+#include <cdt.h>
+#endif
+
+#if defined DISPLAY_ENABLED || defined DISPLAY_FROM_STATUS_ENABLED
+
+
+void Display_Test()
+{
+
+}
+
+
+void Display_MenuInit(void)
+{
+
+}
+
+void Display_DisplayMessage(void)
+{
+
+}
+
+void Display_ShowClear(void)
+{
+
+}
+
+void Display_ShowValueInt(const char *caption, int value)
+{
+
+}
+
+void Display_ShowValueFloat(const char *caption, float value)
+{
+
+}
+
+void Display_ShowValueFloatArr(const char *caption, float value, uint8_t idx)
+{
+
+}
+
+void Display_ShowValueIntArr(const char *caption, int value, uint8_t idx)
+{
+
+}
+
+void Display_MenuShow(int curSel, bool marked)
+{
+
+}
+
+void Display_SetLine(int i, char *text)
+{
+
+}
+
+void Display_Test1(void)
+{
+
+}
+
+void Display_SetSize(int size)
+{
+
+}
+
+void Display_ClearWhite()
+{
+
+}
+
+void Disaypl_ShowNumber(const char *par, int number, int line)
+{
+
+}
+
+void Disaypl_ShowText(const char *par, const char *text, int line)
+{
+
+}
+
+void Display_Redraw()
+{
+
+}
+
+
+#endif
